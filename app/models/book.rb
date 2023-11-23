53e6515542a7f62db6ceac2fac54726473be5cc4
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2023-11-23 01:21:28 +0000

    test

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :user
 
-  validates :title,presence:true
-  validates :body,presence:true,length:{maximum:200}
+
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :body, length: { maximum: 200 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
 
-  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :name, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
 
 

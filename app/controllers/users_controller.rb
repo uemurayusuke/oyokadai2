c94dc370195b1b89f36adc1abcb2c2b0ca366adc
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2023-11-23 02:28:46 +0000

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,24 +14,59 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    unless @user.id == current_user.id
+    redirect_to user_path(current_user.id)
+    end
   end
 
   def update
-    if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
-    else
       @user = User.find(params[:id])
-      @books = @user.books
-      @book = Book.new
-      render :show
+    if @user.update(user_params)
+      redirect_to users_path(@user.id), notice: "You have updated user successfully."
+    else
+      render :edit
     end
   end
+
+
+
+
+  # def update
+  #   is_matching_login_user
+  #   @user = User.find(params[:id])
+  # if @user.update(user_params)
+  #   flash[:notice] = "You have updated user successfully"
+  #   redirect_to user_path(@user.id)
+  # else
+  # render :edit
+  # end
+  # end
+
+
+  # def is_matching_login_user
+  #   user = User.find(params[:id])
+  #   unless user.id == current_user.id
+  #     redirect_to user_path(current_user)
+  #   end
+  # end
+
+
+
 
   private
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
+
+
+  # private
+
+  # def user_params
+  #   params.require(:user).permit(:name, :introduction, :profile_image)
+  # end
+
+
 
   def ensure_correct_user
     @user = User.find(params[:id])

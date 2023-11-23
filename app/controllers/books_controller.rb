433169def07f1fbdf8b73c5ec9e4e97a3921084b
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2023-11-23 05:10:10 +0000

    test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,6 +24,26 @@ class BooksController < ApplicationController
     end
   end
 
+
+
+
+  # def create
+  #   @book = Book.new(book_params)
+  #   @book.user_id = current_user.id
+  #   if @book.save
+  #   flash[:notice] = "You have created book successfully"
+  #   redirect_to book_path(@book.id)
+  #   else
+  #   @books = Book.all
+  #   render :index
+  #   end
+  # end
+
+
+
+
+
+
   def edit
     @book = Book.find(params[:id])
   end

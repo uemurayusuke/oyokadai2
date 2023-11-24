6ecc2085ab67fa9d472e16d2ffec75ecf509ab4d
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2023-11-24 09:13:08 +0000

    [Finish]oyokadai3

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,7 +4,7 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to book_path(book)
+    redirect_to request.referer
 
   end
 
@@ -14,7 +14,7 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to book_path(book)
+    redirect_to request.referer
 
   end
 end

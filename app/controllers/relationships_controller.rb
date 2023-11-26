9d0461cca58f6b8168c4c691da874637bafb3927
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2023-11-26 07:45:13 +0000

    test

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,4 +4,14 @@ class RelationshipsController < ApplicationController
 
   def destroy
   end
+
+ def followings
+    user = User.find(params[:user_id])
+		@users = user.followings
+ end
+
+  def followers
+    user = User.find(params[:user_id])
+		@users = user.followers
+  end
 end

ef5472f77e520dc31369453b24bc65cbba5a2fc0
EC2 Default User <145743053+uemurayusuke@users.noreply.github.com>
2023-11-26 11:48:49 +0000

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
@@ -10,24 +10,10 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
 
 
-
-
-
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
-
-
-# has_many :xxx, class_name: "モデル名", foreign_key: "○○_id", dependent: :destroy
-# has_many :yyy, through: :xxx, source: :zzz
-
-
-
-
-
-
-
 
   validates :name, presence: true
   validates :name, uniqueness: true
@@ -36,12 +22,24 @@ class User < ApplicationRecord
 
 
 
-def get_profile_image(width, height)
+  def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [width, height]).processed
-end
+      profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root :to =>"homes#top"
-  get "home/about"=>"homes#about"
+  root :to =>"home#top"
+  get "home/about"=>"home#about", as: 'about'
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
@@ -16,5 +15,4 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
